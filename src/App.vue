8e510d9b56e79143516bfbec78eda5730dfe8f2a
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-13 17:12:47 +0200

    start header section

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,101 @@
 <template>
-  
+  <div id="app">
+    <header id="site_header">
+      <div class="container d-flex align-items-center justify-content-between">
+        <div id="site_logo">
+          <img
+            src="@/assets/images/light-logo.png"
+            alt="maxcoach-logo"
+            id="header_logo"
+            style="width: 200px"
+          />
+        </div>
+        <div id="site_nav">
+          <ul class="list-unstyled d-flex">
+            <li v-for="(item, index) in navMenu" :key="index">
+              <a :href="item.link">{{ item.text }}</a>
+            </li>
+          </ul>
+        </div>
+        <div class="commands d-flex align-items-center">
+          <font-awesome-icon
+            icon="fa-solid fa-cart-shopping"
+            style="color: white"
+          />
+          <font-awesome-icon
+            icon="fa-regular fa-circle-user"
+            style="color: white"
+          />
+          <input type="search" placeholder="Search" />
+          <font-awesome-icon
+            icon="fa-solid fa-magnifying-glass"
+            class="magnifying-glass"
+          />
+        </div>
+      </div>
+    </header>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  components: {
-    
-  }
-}
+  name: "App",
+  components: {},
+  data() {
+    return {
+      navMenu: [
+        {
+          text: "Home",
+          link: "#",
+        },
+        {
+          text: "Pages",
+          link: "#",
+        },
+        {
+          text: "Courses",
+          link: "#",
+        },
+        {
+          text: "Features",
+          link: "#",
+        },
+        {
+          text: "Shop",
+          link: "#",
+        },
+      ],
+    };
+  },
+};
 </script>
 
 <style lang="scss">
+@import "@/assets/style.scss";
 
+#site_header {
+  background-color: $headerColor;
+
+  .container {
+    column-gap: 1rem;
+
+    .commands {
+      column-gap: 1rem;
+
+      .magnifying-glass {
+        color: $highlightedTextColor;
+        margin-left: -35px;
+      }
+    }
+
+    ul {
+      margin-top: 1rem;
+
+      a {
+        text-decoration: none;
+        color: $mainWhiteColor;
+      }
+    }
+  }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,20 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
+import Vue from "vue";
+import App from "./App.vue";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import {
+  faChevronDown,
+  faCartShopping,
+  faMagnifyingGlass
+} from "@fortawesome/free-solid-svg-icons";
+import { faCircleUser } from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/vue-fontawesome";
+library.add(faChevronDown, faCartShopping, faCircleUser, faMagnifyingGlass);
+Vue.component("font-awesome-icon", FontAwesomeIcon);
+Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
 
-const bootstrap = require("bootstrap")
-Vue.use(bootstrap)
+const bootstrap = require("bootstrap");
+Vue.use(bootstrap);
