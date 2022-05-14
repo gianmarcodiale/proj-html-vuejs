b179e46aaa6aa0da79f806ede0e3b383f1be092d
gianmarcodiale <gianmarcodialessandro@gmail.com>
2022-05-14 18:25:20 +0200

    add header component and jumbotron markup

--- src/App.vue
+++ src/App.vue
@@ -1,46 +1,49 @@
 <template>
   <div id="app">
-    <header id="site_header">
-      <div class="container d-flex align-items-center justify-content-between">
-        <div id="site_logo">
-          <img
-            src="@/assets/images/light-logo.png"
-            alt="maxcoach-logo"
-            id="header_logo"
-            style="width: 200px"
-          />
-        </div>
-        <div id="site_nav">
-          <ul class="list-unstyled d-flex">
-            <li v-for="(item, index) in navMenu" :key="index">
-              <a :href="item.link">{{ item.text }}</a>
-            </li>
-          </ul>
-        </div>
-        <div class="commands d-flex align-items-center">
-          <font-awesome-icon
-            icon="fa-solid fa-cart-shopping"
-            style="color: white"
-          />
-          <font-awesome-icon
-            icon="fa-regular fa-circle-user"
-            style="color: white"
-          />
-          <input type="search" placeholder="Search" />
-          <font-awesome-icon
-            icon="fa-solid fa-magnifying-glass"
-            class="magnifying-glass"
-          />
+    <SiteHeader :navMenu="navMenu" />
+    <div class="jumbotron">
+      <div class="container-fluid">
+        <div class="row" style="height: 670px">
+          <div class="jumbo-text text-center text-white">
+            <h2 class="jumbo-title">Learn At Your Own Pace!</h2>
+            <p>Get teamed up with people of the same will.</p>
+            <button>Get started for free</button>
+          </div>
+          <div class="col left-img text-center">
+            <img src="@/assets/images/home-6-hero-left-person.png" alt="" />
+          </div>
+          <div class="col center-img">
+            <img
+              src="@/assets/images/home-6-hero-poster-final.jpg"
+              alt=""
+              class="screen"
+            />
+            <img
+              src="@/assets/images/icon-youtube-play.png"
+              alt=""
+              class="yt-logo"
+            />
+          </div>
+          <div class="col right-img">
+            <img
+              src="@/assets/images/home-6-hero-right-person-195x300.png"
+              alt=""
+            />
+          </div>
         </div>
       </div>
-    </header>
+    </div>
   </div>
 </template>
 
 <script>
+import SiteHeader from "@/components/SiteHeader.vue";
+
 export default {
   name: "App",
-  components: {},
+  components: {
+    SiteHeader,
+  },
   data() {
     return {
       navMenu: [
@@ -73,28 +76,60 @@ export default {
 <style lang="scss">
 @import "@/assets/style.scss";
 
-#site_header {
+.jumbotron {
   background-color: $headerColor;
 
-  .container {
-    column-gap: 1rem;
+  .jumbo-text {
+    padding: 5rem 0;
 
-    .commands {
-      column-gap: 1rem;
-
-      .magnifying-glass {
-        color: $highlightedTextColor;
-        margin-left: -35px;
-      }
+    button {
+      @include button;
     }
 
-    ul {
-      margin-top: 1rem;
+    .jumbo-title {
+      line-height: 1.17em;
+      font-size: 48px;
+    }
 
-      a {
-        text-decoration: none;
-        color: $mainWhiteColor;
-      }
+    p {
+      color: $jumboParColor;
+    }
+  }
+
+  .center-img {
+    position: relative;
+
+    img.screen {
+      border: 20px solid white;
+      border-radius: 1.25rem;
+      z-index: 1;
+      box-shadow: 1px 8px 5px -2px $shadowColor;
+    }
+
+    img.yt-logo {
+      position: absolute;
+      top: 50%;
+      right: 50%;
+      transform: translate(50%, -50%);
+    }
+  }
+
+  .left-img {
+    position: relative;
+
+    img {
+      width: 370px;
+      position: absolute;
+      left: 5%;
+    }
+  }
+
+  .right-img {
+    position: relative;
+
+    img {
+      position: absolute;
+      bottom: -30%;
     }
   }
 }
--- src/components/SiteHeader.vue
+++ src/components/SiteHeader.vue
@@ -1,18 +1,99 @@
 <template>
-  
+  <header id="site_header">
+    <div class="container d-flex align-items-center justify-content-between">
+      <div id="site_logo">
+        <img
+          src="@/assets/images/light-logo.png"
+          alt="maxcoach-logo"
+          id="header_logo"
+          style="width: 200px"
+        />
+      </div>
+      <div id="site_nav" class="d-flex">
+        <ul class="nav-menu list-unstyled d-flex">
+          <li
+            v-for="(item, index) in navMenu"
+            :key="index"
+            class="d-flex align-items-center"
+          >
+            <a :href="item.link">{{ item.text }}</a>
+            <font-awesome-icon
+              icon="fa-solid fa-chevron-down"
+              style="color: white; width: 10px; padding: 2px"
+            />
+          </li>
+        </ul>
+        <div class="commands d-flex align-items-center">
+          <font-awesome-icon
+            icon="fa-solid fa-cart-shopping"
+            style="color: white"
+          />
+          <font-awesome-icon
+            icon="fa-regular fa-circle-user"
+            style="color: white"
+          />
+          <input type="search" placeholder="Search" />
+          <font-awesome-icon
+            icon="fa-solid fa-magnifying-glass"
+            class="magnifying-glass"
+          />
+        </div>
+      </div>
+    </div>
+  </header>
 </template>
 
 <script>
 export default {
-    name: "SiteHeader",
-    data() {
-        return {
-            
-        }
-    }
+  name: "SiteHeader",
+  props: {
+      navMenu: Object,
+  },
+  data() {
+    return {};
+  },
 };
 </script>
 
 <style lang="scss" scoped>
+#site_header {
+  background-color: $headerColor;
 
+  .container {
+    column-gap: 1rem;
+
+    #site_nav {
+      column-gap: 2rem;
+
+      .nav-menu {
+        column-gap: 3rem;
+      }
+
+      .commands {
+        column-gap: 1rem;
+
+        input {
+          padding: 0.5rem;
+          border: none;
+          border-radius: 0.25rem;
+        }
+
+        .magnifying-glass {
+          color: $highlightedTextColor;
+          margin-left: -40px;
+          cursor: pointer;
+        }
+      }
+
+      ul {
+        margin-top: 1rem;
+
+        a {
+          text-decoration: none;
+          color: $mainWhiteColor;
+        }
+      }
+    }
+  }
+}
 </style>
